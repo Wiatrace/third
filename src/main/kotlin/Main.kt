50f4e140ab843eea9fbb906b8059eae051d2d846
Wiatrace <135695007+Wiatrace@users.noreply.github.com>
2023-09-22 19:52:11 +0300

    third

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("Число в двоичной системе: " + third(readln().toInt()))
+}
+fun third(a: Int): Int {
+    var a = a
+    var b = 0
+    var c = 1
+    var d: Int
+    while (a > 0) {
+        d = a % 2
+        a /= 2
+        b+= d * c
+        c *= 10
+    }
+    return b
+}
